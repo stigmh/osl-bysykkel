f7fa079fd347e6aed9b6b800d26d1fc49faf7e58
Stig M. Halvorsen <halsti@nith.no>
2020-01-16 21:57:34 +0100

    Intial version of Oslo Bysykkel overview

---
+++ gbfs.js
@@ -0,0 +1,108 @@
+var App;
+var Citybike;
+
+const GBFS = 'https://gbfs.urbansharing.com/oslobysykkel.no/';
+
+
+/* Replace weak error handler */
+ErrorHandler = function errorHandler(errorMessage) {
+	if (App) {
+		App.error = errorMessage;
+	} else {
+		alert(errorMessage);
+	}
+};
+
+
+function getData(url, desc) {
+	App.status = 'Henter ' + desc;
+
+	return new Promise(function dataPromise(resolve, reject) {
+		Ajax(url, {
+			success: function(data) {
+				App.progress = undefined;
+				console.log(data);
+				resolve(data);
+			},
+			progress: function(loaded, total) {
+				App.progress = Math.round(total / loaded);
+			},
+			error: function(code, error) {
+				App.status = 'Feil ved henting av ' + desc.toLowerCase();
+				reject((error ? error : 'AJAX error') + '(' + code + ')');
+			}
+		});
+	});
+};
+
+
+function getStations()
+{
+	return getData(GBFS + 'station_information.json', 'stasjoner');
+}
+
+
+function getStatus()
+{
+	return getData(GBFS + 'station_status.json', 'status');
+}
+
+
+function updateDOM(stations, status)
+{
+	App.stations = join(status, stations,
+		'station_id', 'station_id',
+		function(station, stationStatus) {
+			return {
+				name: station.name,
+				address: station.address,
+				lat: station.lat,
+				lon: station.lon,
+				is_installed: stationStatus.is_installed,
+				is_renting: stationStatus.is_renting,
+				is_returning: stationStatus.is_returning,
+				num_bikes_available: stationStatus.num_bikes_available,
+				num_docks_available: stationStatus.num_docks_available,
+				station_id: station.station_id,
+			};
+		}
+	);
+
+	refreshAfter(30);
+}
+
+
+async function update()
+{
+	var stations = await getStations();
+	var status = await getStatus();
+
+	updateDOM(stations.data.stations, status.data.stations);
+}
+
+
+function refreshAfter(seconds)
+{
+	App.status = 'Oppdaterer om ' + seconds + 's';
+
+	if (seconds) {
+		return setTimeout(refreshAfter.bind(null, --seconds), 1000);
+	}
+
+	update();
+}
+
+
+function init(clientIdentifier) {
+	App = new Vue({
+		el: '#app',
+		data: {
+			stations: [],
+			status: 'Initialiserer',
+			error: undefined,
+			progress: undefined
+		}
+	});
+
+	update();
+}
---
+++ util.js
@@ -0,0 +1,118 @@
+/* This could and should be replaced */
+var ErrorHandler = function weakErrorHandler(errorMessage) {
+	alert(errorMessage);
+};
+
+
+/* Can be replaced. Use in final catch() */
+var PromiseCatchHandler = function defaultPromiseCatchHandler(e) {
+	if ((typeof e === 'object') && e.hasOwnProperty('message')) {
+		throw e;
+	} else {
+		throw new Error((typeof e === 'string') ? e : 'Unknown reject');
+	}
+};
+
+
+window.addEventListener('error', function errorListener(e) {
+	e.preventDefault();
+	ErrorHandler(e.message ? e.message : 'Unknown error');
+});
+
+
+window.addEventListener('unhandledrejection', function rejectionListener(e) {
+	var msg = 'Unknown rejection';
+
+	if (e.reason && e.reason.hasOwnProperty('message')) {
+		msg = e.reason.message;
+	}
+
+	ErrorHandler(msg);
+});
+
+
+function Ajax(url, config) {
+	var xmlHTTP = new XMLHttpRequest();
+
+	if ((config === undefined) || typeof config !== 'object') {
+		config = {};
+	}
+
+	if (!('type' in config)) {
+		config.type = 'GET';
+	}
+
+	xmlHTTP.open(config.type, url, true);
+	xmlHTTP.responseType = 'json';
+
+	if ('headers' in config) {
+		var key;
+		for (key of Object.keys(config.headers)) {
+			xmlHTTP.setRequestHeader(key, config.headers[key]);
+		}
+	}
+
+	xmlHTTP.onload = function ajaxOnLoad() {
+		if ((this.status.toString())[0] != 2) {
+			if (config.error !== undefined) {
+				config.error(this.status, 'Bad AJAX request');
+			} else {
+				throw new Error('Bad AJAX request ' + this.code);
+			}
+		}
+
+		if (config.success !== undefined) {
+			config.success(this.response);
+		}
+	};
+
+	xmlHTTP.onprogress = function ajaxOnProgress(e) {
+		if (!e.lengthComputable) {
+			return;
+		}
+
+		if (config.progress !== undefined) {
+			config.progress(e.loaded, e.total);
+		}
+	};
+
+	xmlHTTP.onerror = function ajaxOnError(e) {
+		if (config.error !== undefined) {
+			config.error(this.code, e);
+		} else {
+			throw new Error('AJAX error ' + this.code);
+		}
+	};
+
+	xmlHTTP.send();
+}
+
+
+/* Object join from: https://stackoverflow.com/questions/17500312#17500836 */
+function join(lookupTable, mainTable, lookupKey, mainKey, select) {
+	var l = lookupTable.length;
+	var m = mainTable.length;
+	var lookupIndex = [];
+	var output = [];
+
+	/* Loop through l items */
+	var i;
+	for (i = 0; i < l; i++) {
+		var row = lookupTable[i];
+		/* Create an index for lookup table */
+		lookupIndex[row[lookupKey]] = row;
+	}
+
+	/* loop through m items */
+	for (i = 0; i < m; i++) {
+		var y = mainTable[i];
+
+		/* Get corresponding row from lookupTable */
+		var x = lookupIndex[y[mainKey]];
+
+		/*select only the columns you need*/
+		output.push(select(y, x));
+	}
+
+	return output;
+};
